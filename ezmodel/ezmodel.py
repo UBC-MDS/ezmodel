c82f3b639c92f78af23b423c680194b9c6621c6a
tylercroberts <tcroberts@live.ca>
2018-02-14 11:44:43 -0800

    Setup function skeletons

---
+++ ezmodel/ezmodel.py
@@ -0,0 +1,44 @@
+
+
+def test_train_plot():
+    """"""
+    pass
+
+
+def feature_importance_plot():
+    pass
+
+
+class Score(object):
+    """"""
+
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def mse(self):
+        """"""
+        pass
+
+    def r2(self):
+        """"""
+        pass
+
+    def adj_r2(self):
+        """"""
+        pass
+
+
+def _coerce(x):
+    """Utility function to coerce data into the correct types to be passed to sklearn"""
+    pass
+
+
+
+
+
+
+
+
