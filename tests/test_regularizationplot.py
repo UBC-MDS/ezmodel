5337a34636481cd953e117426d8516242f560b70
Sean Conley <conleyst@gmail.com>
2018-03-10 09:28:43 -0800

    updated tests to cover all branches

--- tests/test_regularizationplot.py
+++ tests/test_regularizationplot.py
@@ -5,9 +5,9 @@ from sklearn.datasets import load_boston, load_breast_cancer
 
 def test_input_model_type():
     """Checks TypeError is raised when input model isn't of allowed type."""
-        X, y = load_boston(return_X_y=True)
-        with pytest.raises(TypeError):
-            regularization_plot(LinearRegression(), alpha=1, X, y)
+    X, y = load_boston(return_X_y=True)
+    with pytest.raises(TypeError):
+        regularization_plot(LinearRegression(), alpha=1, x=X, y=y)
 
 
 def test_nonzero_count_ridge():
@@ -18,7 +18,7 @@ def test_nonzero_count_ridge():
     ridge_models = [Ridge(alpha=a).fit(X,y) for a in alpha_range]
     nz_count = [sum(np.abs(m.coef_)>tol) for m in ridge_models]
 
-    assert nz_count == regularization_plot(Ridge(), alpha=alpha_range, tol=tol, X, y)
+    assert nz_count == regularization_plot(Ridge(), alpha=alpha_range, tol=tol, x=X, y=y)
 
 
 def test_nonzero_count_lasso():
@@ -29,17 +29,34 @@ def test_nonzero_count_lasso():
     lasso_models = [Lasso(alpha=a).fit(X,y) for a in alpha_range]
     nz_count = [sum(np.abs(m.coef_)>tol) for m in lasso_models]
 
-    assert nz_count == regularization_plot(Lasso(), alpha=alpha_range, tol=tol, X, y)
+    assert nz_count == regularization_plot(Lasso(), alpha=alpha_range, tol=tol, x=X, y=y)
 
+def test_nonzero_count_logistic():
+    """Checks using list for alpha with LogisticRegression() outputs correct coefficient counts."""
+    X, y = load_breast_cancer(return_X_y=True)
+    tol=1e-5
+    C_range = [2**i for i in range(-2, 3)]
+    log_models = [LogisticRegression(C=k).fit(X,y) for k in C_range]
+    nz_count = [sum(np.abs(m.coef_[0])>tol) for m in log_models]
+
+    assert nz_count == regularization_plot(Ridge(), alpha=C_range, tol=tol, x=X, y=y)
 
 def test_nonzero_coefs_logistic():
-    """Checks using float for alpha produces correct coefficients for LogisticRegression() model."""
+    """Checks using int for alpha produces correct coefficients for LogisticRegression() model."""
     X, y = load_breast_cancer(return_X_y=True)
     tol=1e-7
-    mod = LogisticRegression(C=10**-7).fit(X,y)
-    mod_coefs = np.abs(mod.coef_[0])
-    for i in range(len(mod_coefs)):
-        if mod_coefs[i] < tol:
-            mod_coefs[i]=0
+    mod = LogisticRegression(C=10.0**-7).fit(X,y)
+    mod_coefs = mod.coef_[0]
+    mod_coefs = [np.abs(c) if c>tol else 0 for c in mod_coefs]
 
-    assert mod_coefs == regularization_plot(LogisticRegression(), alpha=10**7, X, y)
+    assert mod_coefs == regularization_plot(LogisticRegression(), alpha=10.0**7, x=X, y=y)
+
+def test_nonzero_coefs_rigde():
+    """Checks using float for alpha produces correct coefficients for Ridge() model."""
+    X, y = load_boston(return_X_y=True)
+    tol=1e-6
+    mod=Ridge(alpha=2**2.0).fit(X,y)
+    mod_coefs = mod.coef_
+    mod_coefs = [np.abs(c) if c>tol else 0 for c in mod_coefs]
+
+    assert mod_coefs == regularization_plot(Ridge(), alpha=2**2.0, x=X, y=y)
