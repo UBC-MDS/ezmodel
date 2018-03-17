6df384ea592b5abdd8a8a68a2d04e8c546a4aec6
Sean Conley <conleyst@gmail.com>
2018-03-17 11:16:25 -0700

    fixed spacing in setup file

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 import os
 from setuptools import setup, find_packages
 
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return 'Please see: https://github.com/UBC-MDS/ezmodel'
 
-    def read(fname):
-        try:
-            return open(os.path.join(os.path.dirname(__file__), fname)).read()
-        except:
-            return 'Please see: https://github.com/UBC-MDS/ezmodel'
-
-    setup(
-        name='ezmodel',
-        version='v0.1',
-        author=['Alex Kleefeldt', 'Sean Conley', 'Tyler Roberts'],
-        author_email='',
-        description='Simple modelling diagnostic and workflow tools for use with sklearn',
-        long_description=read('README.md'),
-        license='',
-        keywords='machine learning',
-        url='https://github.com/UBC-MDS/ezmodel',
-        packages=find_packages(),
-        install_requires=['os', 'setuptools', 'scipy','numpy', 'sklearn', 'matplotlib'],
-        classifiers=[],
-        include_package_data=True
-    )
+setup(
+    name='ezmodel',
+    version='v0.1',
+    author=['Alex Kleefeldt', 'Sean Conley', 'Tyler Roberts'],
+    author_email='',
+    description='Simple modelling diagnostic and workflow tools for use with sklearn',
+    long_description=read('README.md'),
+    license='',
+    keywords='machine learning',
+    url='https://github.com/UBC-MDS/ezmodel',
+    packages=find_packages(),
+    install_requires=['os', 'setuptools', 'scipy','numpy', 'sklearn', 'matplotlib'],
+    classifiers=[],
+    include_package_data=True
+)
