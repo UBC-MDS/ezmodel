31b73bea3d907cc113a439ca3e4139a9cb0c9e87
Sean Conley <conleyst@gmail.com>
2018-03-17 11:38:29 -0700

    removed import statement

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-def read(fname):
-    try:
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    except:
-        return 'Please see: https://github.com/UBC-MDS/ezmodel'
 
 setup(
     name='ezmodel',
