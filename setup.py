158e7b6a9c3f5421616303ec778b8a4a6d4c83c6
Sean Conley <31637222+conleyst@users.noreply.github.com>
2018-03-18 09:32:47 -0700

    removed sys from required packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     keywords='machine learning',
     url='https://github.com/UBC-MDS/ezmodel',
     packages=find_packages(),
-    install_requires=['sys', 'setuptools', 'scipy','numpy', 'sklearn', 'matplotlib', 'pandas'],
+    install_requires=['setuptools', 'scipy','numpy', 'sklearn', 'matplotlib', 'pandas'],
     classifiers=[],
     include_package_data=True
 )
